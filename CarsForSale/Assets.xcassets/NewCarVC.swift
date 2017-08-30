26b0bc561a6daeb8dc42b311237301529ae35f9c
nish8192 <nish8192@gmail.com>
2017-08-30 09:09:12 -0400

    Number pad appears when price text field is selected. Input is automatically formatted to currency

--- CarsForSale/Assets.xcassets/NewCarVC.swift
+++ CarsForSale/Assets.xcassets/NewCarVC.swift
@@ -33,6 +33,8 @@ class NewCarVC: UIViewController {
         }
     }
     
+    
+    
     private func saveCar(car: Car){
         self.loadData();
         cars.append(car);
@@ -84,8 +86,14 @@ class NewCarVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.newCarPrice.keyboardType = .numberPad;
+        self.newCarPrice.addTarget(self, action: #selector(newCarPriceDidChange), for: .editingChanged)
+    }
+    
+    func newCarPriceDidChange(_ textField: UITextField) {
+        if let amount = textField.text?.currencyInputFormatting() {
+            textField.text = amount;
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -104,4 +112,32 @@ class NewCarVC: UIViewController {
     }
     */
 
+}
+
+extension String {
+    func currencyInputFormatting() -> String {
+        
+        var number: NSNumber!
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currencyAccounting
+        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        
+        var amountWithPrefix = self
+        
+        // remove from String: "$", ".", ","
+        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count), withTemplate: "")
+        
+        let double = (amountWithPrefix as NSString).doubleValue
+        number = NSNumber(value: (double / 100))
+        
+        // if first number is 0 or all numbers were deleted
+        guard number != 0 as NSNumber else {
+            return ""
+        }
+        
+        return formatter.string(from: number)!
+    }
 }
--- CarsForSale/CarListTableVC.swift
+++ CarsForSale/CarListTableVC.swift
@@ -61,7 +61,6 @@ class CarListTableVC: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return (cars.count);
     }
 
